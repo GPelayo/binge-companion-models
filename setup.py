4129145d7b1b0a18000774415dc62a83b854ea39
gpelayo <gpelayo109@gmail.com>
2022-07-11 14:37:48 -0700

    Added models and setup script

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='Binge Companion Models',
       version='0.1',
@@ -8,5 +8,5 @@ setup(name='Binge Companion Models',
       author='Geryl Pelayo',
       author_email='hi@gerylpelayo.com',
       packages=['binge_models'],
-      install_requires=["<sqlalchemy>"],
+      install_requires=["sqlalchemy>=1.4.39"],
       )
